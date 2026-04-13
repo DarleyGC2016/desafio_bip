d8861d837e66ee52be86a1ebbd964f131c1f3f07
DarleyGC2016 <garciacardoso.darley@gmail.com>
2026-04-13 12:23:25 -0300

    Atualizei o BackendApplication

--- backend-module/src/main/java/com/example/backend/BackendApplication.java
+++ backend-module/src/main/java/com/example/backend/BackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = {"com.example"})
+@SpringBootApplication(scanBasePackages = {"com.example", "com.example.ejb"})
 @EntityScan(basePackages = "com.example")
 public class BackendApplication {
     public static void main(String[] args) {
