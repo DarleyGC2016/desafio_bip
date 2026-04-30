760817265b7a3b0e4736d8e17addc3eac850cbe0
DarleyGC2016 <garciacardoso.darley@gmail.com>
2026-04-30 13:05:44 -0300

    task 05 -  adicionado o toolbar e alguns ajusttes

--- frontend/beneficio-web/src/app/app.routes.ts
+++ frontend/beneficio-web/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { BeneficioListComponent } from './features/beneficio/beneficio-list/beneficio-list.component';
 import { BeneficioDetailComponent } from './features/beneficio/beneficio-detail/beneficio-detail.component';
+import { HomeComponent } from './features/page/home/home.component';
 
 
 export const routes: Routes = [
-    {path:'', component: BeneficioListComponent},
+    {path:'', component: HomeComponent},
     {path: 'beneficios', component: BeneficioListComponent},
     {path: 'beneficios/:id', component: BeneficioDetailComponent}
 ];
---
+++ frontend/beneficio-web/src/app/shared/components/nav-bar/nav-bar/nav-bar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'nav-bar',
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    RouterLink,
+    MatListModule
+  ],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css',
+})
+export class NavBarComponent { 
+  showFiller = false;
+}
